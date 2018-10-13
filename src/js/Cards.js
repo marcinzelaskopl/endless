1c574d0202cf65f63e3084027a913b53c327c544
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-10-13 19:27:19 +0200

    added in out animations

--- src/js/Cards.js
+++ src/js/Cards.js
@@ -2,24 +2,70 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Cards extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            unfallen: 'none',
+            riftborn: 'none',
+            vodyani: 'none'
+        }
+    }
+
+    handleEnterUnfallen = () => {
+        this.setState({
+            unfallen: 'block',
+        })
+    };
+
+    handleLeaveUnfallen = () => {
+        this.setState({
+            unfallen: 'none',
+        })
+    };
+    handleEnterRiftborn = () => {
+        this.setState({
+            riftborn: 'block',
+        })
+    };
+
+    handleLeaveRiftborn = () => {
+        this.setState({
+            riftborn: 'none',
+        })
+    };
+    handleEnterVodyani = () => {
+        this.setState({
+            vodyani: 'block',
+        })
+    };
+
+    handleLeaveVodyani = () => {
+        this.setState({
+            vodyani: 'none',
+        })
+    };
     render(){
         return (
             <div className={'grid-container'}>
                 <div className={'row'}>
                     <div className={'col-1'}>
                     </div>
-                    <div className={'col-3 cards unfallen'}>
-                        <h1>The Unfallen</h1>
+                    <div onMouseLeave={this.handleLeaveUnfallen} onMouseEnter={this.handleEnterUnfallen} className={'col-3 cards' +
+                    ' unfallen'}>
+                        <h1 style={{display: this.state.unfallen}}>Unfallen</h1>
                     </div>
                     <div className={'col-1'}>
                     </div>
-                    <div className={'col-3 cards riftborn'}>
-                        <h1>The Riftborn</h1>
+                    <div onMouseLeave={this.handleLeaveRiftborn} onMouseEnter={this.handleEnterRiftborn} className={'col-3 cards' +
+                    ' riftborn'}>
+                        <h1 style={{display: this.state.riftborn}}>Riftborn</h1>
                     </div>
                     <div className={'col-1'}>
                     </div>
-                    <div className={'col-3 cards vodyani'}>
-                        <h1>The Vodyani</h1>
+                    <div onMouseLeave={this.handleLeaveVodyani} onMouseEnter={this.handleEnterVodyani} className={'col-3 cards' +
+                    ' vodyani'}>
+                        <h1 style={{display: this.state.vodyani}}>Vodyani</h1>
                     </div>
                     <div className={'col-1'}>
                     </div>
